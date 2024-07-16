8d3c8f9931f1511af9aa57470f0f01e07f5fd7e9
Hff <Hff@email.com>
2024-07-16 13:23:37 +0800

    update remote python script

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
+import sys
+
 import execjs
 import requests
 
-def get_code(url):
-    code = requests.get(url).text
-    return code
-def get_fas_trace_Id():
-    trace_id = execjs.compile(get_code("https://raw.githubusercontent.com/Javer-Hff/SpidersRemote/main/js/tujia/utils.js")).call("w", 8)
-    return trace_id;
+
+class SpidersRemote:
+    def __init__(self):
+        pass
+
+    def get_code(slef, url):
+        code = requests.get(url).text
+        return code
+
+    def get_fas_trace_id(self):
+        trace_id = execjs.compile(
+            self.get_code("https://raw.githubusercontent.com/Javer-Hff/SpidersRemote/main/js/tujia/utils.js")).call("w",8)
+        return trace_id
 
 
+function_map = {
+    "getFasTraceId": SpidersRemote().get_fas_trace_id
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    function_name = sys.argv[1]
+    print(function_map[function_name]())
